c94bace581688e9e3af8eb5661b1421b65ffc4ef
BESNARD Jean-Baptiste <jbbesnard@paratools.com>
2024-03-05 16:12:00 +0100

    EX3 leak

---
+++ ex3/leak.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_SEGMENTS 5
+#define SEGMENT_SIZE 100
+
+int main() {
+    int *segments[NUM_SEGMENTS];
+
+    for (int i = 0; i < NUM_SEGMENTS; i++) {
+        segments[i] = (int *)malloc(SEGMENT_SIZE * sizeof(int));
+        if (segments[i] == NULL) {
+            printf("Erreur lors de l'allocation de mémoire pour le segment %d.\n", i);
+            for (int j = 0; j < i; j++) {
+                free(segments[j]);
+            }
+            return -1;
+        }
+    }
+
+    for (int i = 0; i < NUM_SEGMENTS; i++) {
+        for (int j = 0; j < SEGMENT_SIZE; j++) {
+            segments[i][j] = i + j;
+        }
+    }
+
+    for (int i = 0; i < NUM_SEGMENTS; i ++) {
+        free(segments[i]);
+        segments[++i] = NULL;
+    }
+
+    return 0;
+}
+
