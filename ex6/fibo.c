fb65eebf3dfc48707a36604e6c73ff1191226eec
BESNARD Jean-Baptiste <jbbesnard@paratools.com>
2024-03-05 16:39:06 +0100

    broken fibo

---
+++ ex6/fibo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+unsigned long long fibonacci(unsigned long long n) {
+    if (n < 0) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main() {
+    unsigned long long n = 50; // Valeur de n choisie arbitrairement
+
+    printf("Calcul du nombre de Fibonacci pour n = %llu\n", n);
+    printf("Le résultat est : %llu\n", fibonacci(n));
+
+    return 0;
+}
