d285b5239b6a4bbfe3f032c270f6a4a66dcee998
BESNARD Jean-Baptiste <jbbesnard@paratools.com>
2024-03-05 16:38:29 +0100

    Ex4

---
+++ ex4/somme.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int array[5] = {1, 2, 3, 4, 5};
+    int sum = 0;
+
+    // Calculer la somme des éléments du tableau
+    for (int i = 0; i <= 5; i++) { // Bug: Utilisation de <= au lieu de <
+        sum += array[i];
+    }
+
+    printf("La somme des éléments du tableau est : %d\n", sum);
+
+    return 0;
+}
+
